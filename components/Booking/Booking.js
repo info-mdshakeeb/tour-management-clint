a49c7eb9e015436f60d994040336e9fb63080405
itzshakeeb <itzshakeeb@gmail.com>
2023-07-25 13:31:27 +0600

    complite booking full

--- components/Booking/Booking.js
+++ components/Booking/Booking.js
@@ -1,11 +1,35 @@
 'use client'
+import { useState } from "react";
 import { AiOutlineStar } from "react-icons/ai";
 
 const Booking = ({ tour }) => {
+    const [person, setPerson] = useState(1)
+    const [selectedDate, setSelectedDate] = useState(new Date().toISOString().slice(0, 10))
+    let price = tour.price * person
+    let services = tour.price * 10 / 100 * person
+    let total = (tour.price + services) * person
 
+    const handleBooking = (e) => {
+        e.preventDefault()
+
+        // Check if the selected date is less than the present day
+        const selectedDateObj = new Date(selectedDate);
+        const presentDate = new Date();
+        if (selectedDateObj < presentDate) {
+            alert("Selected date cannot be less than the present day.");
+            return;
+        }
+
+        const form = e.target
+        const name = form.name.value
+        const phone = form.phone.value
+        const date = form.date.value
+        const bookingData = { name, phone, date, person }
+        console.log(bookingData);
+    }
     return (
         <div className="col-span-12 md:col-span-4 sticky top-16 max-h-[460px] ">
-            <div className="p-4 border">
+            <form onSubmit={handleBooking} className="p-4 border rounded-md shadow-sm">
                 <div className="flex items-center justify-between ">
                     <p className="text-md"><span className="text-orange-400 text-2xl font-bold">{tour?.price}$ </span> /per person</p>
                     <div className="flex gap-2 items-center">
@@ -24,9 +48,11 @@ const Booking = ({ tour }) => {
                         <input required maxLength={30} placeholder="Name" name="name" type="text" className="w-full outline-none border-b" />
                         <input required maxLength={30} placeholder="Phone Number" name="phone" type="number" className="w-full outline-none border-b" />
                         <div className="flex gap-2">
-                            <input required maxLength={30} placeholder="Date" name="date" type="date" className="w-full outline-none border-b" />
                             <input
-
+                                onChange={(e) => setSelectedDate(e.target.value)}
+                                required placeholder="Date" name="date" type="date" className="w-full outline-none border-b" />
+                            <input
+                                onChange={(e) => setPerson(e.target.value)}
                                 required maxLength={3} placeholder="Person" name="person" type="number" className="w-full outline-none border-b" />
                         </div>
                     </div>
@@ -34,15 +60,15 @@ const Booking = ({ tour }) => {
                         <div className="flex justify-between">
                             <div className="flex gap-2">
                                 <span className="text-orange-400  font-bold">$  {tour?.price}</span>
-                                * <span className="text-orange-400  font-bold">1 person </span>
+                                * <span className="text-orange-400  font-bold">{person} person </span>
                             </div>
-                            <span className="text-orange-400  ">{tour?.price}$ </span>
+                            <span className="text-orange-400  ">{price.toFixed(2)}$ </span>
                         </div>
                         <div className="flex justify-between">
                             <div className="flex gap-2">
                                 <p>Services charge</p>
                             </div>
-                            <span className="text-orange-400  ">{tour?.price}$ </span>
+                            <span className="text-orange-400  ">{services.toFixed(2)}$ </span>
                         </div>
                         <hr />
                         <div className="flex justify-between">
@@ -50,12 +76,20 @@ const Booking = ({ tour }) => {
                             <div className="flex gap-2">
                                 <p>Total</p>
                             </div>
-                            <span className="text-orange-400 ">{tour?.price}$ </span>
+                            <span className="text-orange-400 ">{total.toFixed(2)}$ </span>
                         </div>
                     </div>
                 </div>
-                <button className="btn btn-warning w-full mt-4 btn-sm">book Now</button>
-            </div>
+                <div className="duration-300">
+                    {person > 0 ?
+                        <button
+                            className="btn btn-warning w-full mt-4 btn-sm">book Now</button>
+                        :
+                        <button
+                            className="btn btn-warning w-full mt-4 btn-sm" disabled>book Now</button>
+                    }
+                </div>
+            </form>
 
         </div>
     );
